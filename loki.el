31a8a7c1f3118d7f8f9a0416a260c4d33cdc6be8
Diego Fernández Fernández <diego@simplelogica.net>
2009-07-07 11:15:50 +0200

    Theme changed to twilight and added function to resize frame on startup.

--- loki.el
+++ loki.el
@@ -1,6 +1,6 @@
 ;;(require 'color-theme)
-(load-file "~/.emacs.d/elpa-to-submit/zenburn.el")
-(color-theme-zenburn)
+(load-file "~/.emacs.d/elpa-to-submit/twilight.el")
+(color-theme-twilight)
 
 (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 100)
 
@@ -16,3 +16,22 @@
 ;;(add-to-list 'yas/extra-mode-hooks 'html-mode-hook)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/elpa-to-submit/snippets/")
+
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+        (add-to-list 'default-frame-alist (cons 'width 120))
+      (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist 
+                 (cons 'height (/ (- (x-display-pixel-height) 200) (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
