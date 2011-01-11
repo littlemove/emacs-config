78f35afe957b4d152db99b814ab77f20d71eed0d
Diego Fernández Fernández <diego@simplelogica.net>
2011-01-11 18:29:02 +0100

    Removing code to go fullscreen using wmctrl and window size based on resolution

--- loki.el
+++ loki.el
@@ -1,37 +1,9 @@
-;;(require 'color-theme)
 (load-file "~/.emacs.d/elpa-to-submit/twilight.el")
 (color-theme-twilight)
 
-;;(set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 100)
-
-;; Toogle full-screen mode (needs wmctrl installed)
-(defun full-screen-toggle ()
-  "toggle full-screen mode"
-  (interactive)
-  (shell-command "wmctrl -r :ACTIVE: -btoggle,fullscreen"))
-
-(global-set-key (kbd "<f11>")  'full-screen-toggle)
+(set-face-attribute 'default nil :family "Anonymous Pro" :height 150)
 
 (require 'yasnippet) ;; not yasnippet-bundle
-;;(add-to-list 'yas/extra-mode-hooks 'html-mode-hook)
+(add-to-list 'yas/extra-mode-hooks 'html-mode-hook)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/elpa-to-submit/snippets/")
-
-(defun set-frame-size-according-to-resolution ()
-  (interactive)
-  (if window-system
-  (progn
-    ;; use 120 char wide window for largeish displays
-    ;; and smaller 80 column windows for smaller displays
-    ;; pick whatever numbers make sense for you
-    (if (> (x-display-pixel-width) 1280)
-        (add-to-list 'default-frame-alist (cons 'width 120))
-      (add-to-list 'default-frame-alist (cons 'width 80)))
-    ;; for the height, subtract a couple hundred pixels
-    ;; from the screen height (for panels, menubars and
-    ;; whatnot), then divide by the height of a char to
-    ;; get the height we want
-    (add-to-list 'default-frame-alist 
-                 (cons 'height (/ (- (x-display-pixel-height) 200) (frame-char-height)))))))
-
-(set-frame-size-according-to-resolution)
