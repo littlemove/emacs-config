c868f051ff07f046c5772cdbb02a90274ec68089
Diego Fernández Fernández <diego@simplelogica.net>
2011-01-11 18:31:12 +0100

    Reorganicing custom code. Out from the files from starter-kit

---
+++ loki/ruby.el
@@ -0,0 +1,23 @@
+;; rhtml-mode
+(add-to-list 'load-path "~/.emacs.d/elpa-to-submit/rhtml")
+(require 'rhtml-mode)
+(add-hook 'rhtml-mode-hook
+         (lambda () (rinari-launch)))
+
+;;;; Hide/show blocks of code on ruby mode
+(defun ruby-custom-setup ()
+  (add-to-list 'hs-special-modes-alist
+               '(ruby-mode "\\(def\\|do\\)""end""#"
+                           (lambda (arg) (ruby-end-of-block))
+                           nil
+                           ))
+
+  (local-set-key (kbd "C-c <right>") 'hs-show-block)
+  (local-set-key (kbd "C-c <left>")  'hs-hide-block)
+  (local-set-key (kbd "C-c <up>")    'hs-hide-all)
+  (local-set-key (kbd "C-c <down>")  'hs-show-all)
+
+  (hs-minor-mode t)
+  )
+
+(add-hook 'ruby-mode-hook 'ruby-custom-setup)
