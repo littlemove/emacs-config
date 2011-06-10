d828204f30590bf12b87278e801b25d41799ce15
Diego Fernández Fernández <diego@simplelogica.net>
2011-06-10 14:47:52 +0200

    Flyspell config: enable/disable modes

--- diego.el
+++ diego.el
@@ -1,14 +1,23 @@
 (push "/usr/local/bin" exec-path)
 
+;; Flyspell related config
 (setq ispell-program-name "aspell")
 (setq ispell-list-command "list")
+
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
+(dolist (hook '(change-log-mode-hook log-edit-mode-hook html-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode -1))))
+
 (global-set-key (kbd "<C-M-tab>") 'flyspell-auto-correct-word)
 
+;; Theme, fonts, ...
 (load-file "~/.emacs.d/elpa-to-submit/twilight.el")
 (color-theme-twilight)
 
 (set-face-attribute 'default nil :family "Anonymous Pro" :height 150)
 
+;; Snippets (using yasnippets)
 (require 'yasnippet) ;; not yasnippet-bundle
 (add-to-list 'yas/extra-mode-hooks 'html-mode-hook)
 (yas/initialize)
