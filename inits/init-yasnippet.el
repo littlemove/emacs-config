e176194fec0d93db06ec8582369e64cf73a01e97
Diego Fernandez Fernandez <diego@simplelogica.net>
2012-03-07 19:02:04 +0100

    Yassnipet adjustments.

--- inits/init-yasnippet.el
+++ inits/init-yasnippet.el
@@ -1,3 +1,4 @@
-(setq yas/snippet-dirs '("~/.emacs.d/snippets/" "~/.emacs.d/el-get/yasnippet/snippets/"))
-(mapc 'yas/load-directory yas/snippet-dirs)
+(setq yas/root-directory '("~/.emacs.d/snippets/"
+                           "~/.emacs.d/el-get/yasnippet/snippets/"))
+(mapc 'yas/load-directory yas/root-directory)
 (yas/global-mode)
