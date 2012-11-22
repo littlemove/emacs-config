7d620f796002ac7298da7e5c4023a768fab30a5e
Diego Fernandez Fernandez <diego.fernandez.fernandez@gmail.com>
2012-11-22 18:11:18 +0100

    Changing the way el-get gets initialized.

--- init.el
+++ init.el
@@ -47,13 +47,14 @@
 ;; el-get setup
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
-   (lambda (s)
-     (let (el-get-master-branch)
-       (end-of-buffer)
-       (eval-print-last-sexp)))))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get 'sync)
 
 (setq el-get-byte-compile nil)
 (setq el-get-user-package-directory "~/.emacs.d/inits")
